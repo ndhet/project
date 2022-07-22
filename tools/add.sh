f5871f01d6e008707be197dcb60ece62a7e9f591
KHAIRUNNISAA AZMII PUTRI PERMATA <107531486+henceutbeey@users.noreply.github.com>
2022-07-22 21:26:28 +0700

    Create add.sh

---
+++ tools/add.sh
@@ -0,0 +1,8 @@
+USER="dedi"
+PASS="Dedi100"
+passwd $USER
+read "Enter new UNIX password: "
+write "$PASS\n"
+read "Retype new UNIX password: "
+write "$PASS\n"
+read "password updated successfully"
