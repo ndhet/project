db48dd0576e2b948ab3ebf60b4aeb5d35f1ca66f
SAMBO WIBOWO <107531486+ndhet@users.noreply.github.com>
2024-08-13 23:21:19 +0700

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -41,7 +41,7 @@ exit /b 2)
 net user Administrator $PASSADMIN
 
 
-netsh -c interface ip set address name="$IFACE" static address=$IP4 mask=255.255.240.0 gateway=$GW
+netsh -c interface ip set address name="$IFACE" static $IP4 255.255.240.0 $GW
 netsh -c interface ip add dnsservers name="$IFACE" address=1.1.1.1 index=1 validate=no
 netsh -c interface ip add dnsservers name="$IFACE" address=8.8.4.4 index=2 validate=no
 
@@ -102,4 +102,4 @@ cp -f /tmp/dpart.bat dpart.bat
 
 echo 'Your server will turning off in 3 second'
 sleep 3
-poweroff
+
