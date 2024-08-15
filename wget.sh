47d9e4ea3347d6803b6bb62cd60eab6eb49fb374
SAMBO WIBOWO <107531486+ndhet@users.noreply.github.com>
2024-08-15 23:03:40 +0700

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -83,7 +83,7 @@ wget --no-check-certificate -O- $PILIHOS | gunzip | dd of=/dev/vda bs=3M status=
 sudo mkdir /tmp/windows
 sudo ntfsfix /dev/vda1
 sudo mount -o rw /dev/vda1 /tmp/windows
-cd "/tmp/windows/ProgramData/Microsoft/Windows/Start\ Menu/Programs/StartUp/"
+cd /tmp/windows/ProgramData/Microsoft/Windows/Start\ Menu/Programs/StartUp/
 cp -f /tmp/net.bat net.bat
 clear
 
