7a1be88e52e3a9941adf03f15c256c2f2059dc62
SAMBO WIBOWO <107531486+ndhet@users.noreply.github.com>
2024-08-15 21:31:20 +0700

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -2,23 +2,47 @@
 #
 # CREATED By SSHAXOR.MY.ID
 #
-echo "Pilih OS yang ingin anda install"
-echo "	1) Windows 2019(Default)"
-echo "	2) Windows 2016"
-echo "	3) Windows 2012"
-echo "	4) Windows 10"
-echo "	5) Windows 2022"
-echo "	6) Windows 11"
-echo "	7) Pakai link gz mu sendiri"
-
-read -p "Pilih [1]: " PILIHOS
+export NC="\e[0m"
+export YELLOW='\033[0;33m';
+export RED="\033[0;31m"
+export GREEN='\033[0;32m'
+export COLBG='\E[40;1;41m'
+echo -e "$RED┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$RED${NC}${COLBG}           • AUTO SCRIPT INSTALL RDP •           ${NC}$RED$NC"
+echo -e "$RED└─────────────────────────────────────────────────┘${NC}"
+echo -e "$RED┌─────────────────────────────────────────────────┐${NC}"
+tram=$( free -h | awk 'NR==2 {print $2}' )
+uram=$( free -h | awk 'NR==2 {print $3}' )
+IPVPS=$(curl -s ipinfo.io/ip)
+ISP=$(curl -s https://ipapi.co/$IPVPS/org)
+CITY=$(curl -s https://ipapi.co/$IPVPS/city)
+echo -e "$RED $NC Memory Usage   : $uram / $tram"
+echo -e "$RED $NC ISP & City     : $ISP & $CITY"
+echo -e "$RED $NC IP-VPS         : ${RED}$IPVPS${NC}"
+echo -e "$RED└─────────────────────────────────────────────────┘${NC}"
+echo -e "$RED┌─────────────────────────────────────────────────┐${NC}"
+echo -e "  ${GREEN}[01]${NC} • Windows 2012  ${GREEN}[05]${NC} • Windows 10"
+echo -e "  ${GREEN}[02]${NC} • Windows 2016  ${GREEN}[06]${NC} • Windows 11"
+echo -e "  ${GREEN}[03]${NC} • Windows 2019  ${GREEN}[07]${NC} • Custom Link GZ"
+echo -e "  ${GREEN}[04]${NC} • Windows 2022  ${GREEN}[00]${NC} • Batal"
+echo -e "$RED└─────────────────────────────────────────────────┘${NC}"
+echo -e "$RED┌─────────────────────────────────────────────────┐$NC"
+echo -e "$RED $NC Version     :${RED} 1.0 Beta Version${NC}"
+echo -e "$RED $NC Client Name : Dedi Humaedi"
+echo -e "$RED $NC License     : LifeTime"
+echo -e "$RED└─────────────────────────────────────────────────┘$NC"
+echo -e "$RED┌────────────────────── BY ───────────────────────┐${NC}"
+echo -e "$RED ${NC}                  • SSHAXOR •                  $RED $NC"
+echo -e "$RED└─────────────────────────────────────────────────┘${NC}"
+echo -e ""
+read -p "  Pilih [>]: " PILIHOS
 
 case "$PILIHOS" in
 	1|"") PILIHOS="http://167.172.78.82/GPISERVER2019.gz"  IFACE="Ethernet Instance 0";;
 	2) PILIHOS="http://167.172.78.82/GPISERVER2016.gz"  IFACE="Ethernet Instance 0";;
 	3) PILIHOS="https://hadongpho.com/css/gz/windows2012.gz"  IFACE="Ethernet Instance 0";;
-	4) PILIHOS="https://hadongpho.com/css/gz/windows10.gz"  IFACE="Ethernet Instance 0 2";;
-	5) PILIHOS="http://167.172.78.82/GPISERVER2022.gz"  IFACE="Ethernet Instance 0";;
+	4) PILIHOS="http://167.172.78.82/GPISERVER2022.gz"  IFACE="Ethernet Instance 0 2";;
+	5) PILIHOS="https://hadongpho.com/css/gz/windows10.gz"  IFACE="Ethernet Instance 0";;
 	6) PILIHOS="https://hadongpho.com/css/gz/windows11.gz"  IFACE="Ethernet Instance 0 2";;
 	7) read -p "Masukkan Link GZ mu : " PILIHOS;;
 	*) echo "pilihan salah"; exit;;
@@ -40,12 +64,12 @@ echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas",
 "%temp%\Admin.vbs"
 del /f /q "%temp%\Admin.vbs"
 exit /b 2)
-net user Administrator $PASSADMIN
+net user sshaxor $PASSADMIN
 
 
 netsh -c interface ip set address name="$IFACE" static $IP4 255.255.240.0 $GW
-netsh -c interface ip add dnsservers name="$IFACE" address=1.1.1.1 index=1 validate=no
-netsh -c interface ip add dnsservers name="$IFACE" address=8.8.4.4 index=2 validate=no
+netsh -c interface ip add dnsservers name="$IFACE" address=8.8.8.8 index=1 validate=no
+netsh -c interface ip add dnsservers name="$IFACE" address=1.1.1.1 index=2 validate=no
 
 cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
 del /f /q net.bat
